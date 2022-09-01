8e34fdcf564b53c6273e48d1c8c0519bcb804c54
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-09-01 14:02:13 -0700

    Created Stack

--- StackaAndQueue/StackaAndQueue/Program.cs
+++ StackaAndQueue/StackaAndQueue/Program.cs
@@ -2,11 +2,16 @@
 
 namespace StackaAndQueue
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Stacks and Queues program");
+            LinkedList list = new LinkedList();            
+            list.Push(70);
+            list.Push(30);
+            list.Push(56);
+            list.Display();
         }
     }
 }

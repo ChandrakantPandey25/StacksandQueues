1b1d9d1ab5e3b03e6f397268a172dd46c4a78ab0
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-09-01 14:07:51 -0700

    modified

---
+++ StackaAndQueue/StackaAndQueue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StackaAndQueue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stacks and Queues program");
+            LinkedList list = new LinkedList();            
+            list.Push(70);
+            list.Push(30);
+            list.Push(56);
+            list.Display();
+        }
+    }
+}

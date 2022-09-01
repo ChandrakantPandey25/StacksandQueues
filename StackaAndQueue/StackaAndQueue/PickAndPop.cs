26aca0897de9ab7e1a49809a4a8b11a1afd69276
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-09-01 14:18:05 -0700

    Added Peak and Pop

---
+++ StackaAndQueue/StackaAndQueue/PickAndPop.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackaAndQueue
+{
+    class PickAndPop
+    {
+        private Node top;
+        public PickAndPop()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        public void Peak()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack ", this.top.data);
+        }
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
+        internal void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peak();
+                Pop();
+            }
+            Console.WriteLine("Stack is empty now.");
+        }
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Node n = top;
+                while (n.next != null)
+                {
+                    Console.Write(n.data + " ");
+                    n = n.next;
+                }
+                Console.Write(n.data + "\n");
+            }
+        }
+    }
+}
--- StackaAndQueue/StackaAndQueue/Program.cs
+++ StackaAndQueue/StackaAndQueue/Program.cs
@@ -7,11 +7,31 @@ namespace StackaAndQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stacks and Queues program");
-            LinkedList list = new LinkedList();            
-            list.Push(70);
-            list.Push(30);
-            list.Push(56);
-            list.Display();
+            Console.WriteLine("Enter 1 to create stack");
+            Console.WriteLine("Enter 2 for Pick and Pop from stack");
+            switch(Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    LinkedList list = new LinkedList();
+                    list.Push(70);
+                    list.Push(30);
+                    list.Push(56);
+                    list.Display();
+                    break;
+                case 2:
+                    PickAndPop peak = new PickAndPop();
+                    peak.Push(70);
+                    peak.Push(30);
+                    peak.Push(56);
+                    peak.IsEmpty();
+                    peak.Display();
+                    break;
+                default:
+                    Console.WriteLine("Enter Valid number");
+                    break;
+
+            }
+
         }
     }
 }
